28f412e79e4391e0a93a835e906e4104ed19d9bd
Marcus Kertesz <ModernMAK@Hotmail.com>
2015-05-04 10:55:06 -0700

    Update GambleCard.cs

--- GamblingFramework/GamblingFramework/Card/GambleCard.cs
+++ GamblingFramework/GamblingFramework/Card/GambleCard.cs
@@ -3,28 +3,32 @@
 namespace GamblingFramework
 {
     [Serializable]
-    public class GambleCard : BaseCard, IGambleCard
+    public class GambleCard : BaseCard, IStandardCard
     {
+        /**
+        Seperating Data from Display, if we dont have a valid card, our renderer will notify us, so no clamping.
+        
         /// <summary>
         /// The Minimum for a GambleCard is 0 (The Ace of Spades)
         /// </summary>
         public const int
-            CARD_INDEX_MIN = 0;
+            CARD_IDENTITY_MIN = 0;
 
         /// <summary>
         /// The Maximum for a GambleCard deck without Jokers is 51 (The King of Diamonds)
         /// </summary>
         public const int
-            CARD_INDEX_GAMBLE_MAX = 51;
+            CARD_IDENTITY_MAX = 51;
 
         /// <summary>
         /// The Maximum for a GambleCard deck with Jokers is 53 (The Red Joker)
         /// </summary>
         public const int
             CARD_INDEX_STANDARD_MAX = 53;
-
+        **/
+        
         /// <summary>
-        /// Creates a GambleCard with an Index of 0 (representing the Ace of Spades)
+        /// Creates a GambleCard with an Identity of 0 (representing the Ace of Spades)
         /// </summary>
         public GambleCard()
             : this(0)
@@ -32,26 +36,29 @@ namespace GamblingFramework
         }
 
         /// <summary>
-        /// Creates a GambleCard with a given Index, if no index is given, it will default to an Index of 0 (representing the Ace of Spades)
+        /// Creates a GambleCard with a given Identity, if no identity is given, it will default to an Identity of 0 (representing the Ace of Spades)
         /// </summary>
-        public GambleCard(int index = CARD_INDEX_MIN)
-            : base(index)
+        public GambleCard(int identity)
+            : base(identity)
         {
         }
 
+        /**
+        No longer neccessary, contemplate removing Identity as a virtual member
+        
         /// <summary>
         /// Gets or Sets the GambleCard
-        /// When set, the Index will be bounded to 0 and 53 (Ace of Spades and the Red Joker)
+        /// When set, the Identity will be bounded to 0 and 53 (Ace of Spades and the Red Joker)
         /// </summary>
-        public override int Index
+        public override int Identity
         {
             get
             {
-                return base.Index;
+                return base.Identity;
             }
             set
             {
-                base.Index =
+                base.Identity =
                     Math.Clamp(
                         value,
                         CARD_INDEX_MIN,
@@ -59,9 +66,10 @@ namespace GamblingFramework
                     );
             }
         }
+        */
 
         /// <summary>
-        /// Gets the Suit for this Index
+        /// Gets the Suit for this Identity
         /// </summary>
         public GambleCardSuit Suit
         {
@@ -72,7 +80,7 @@ namespace GamblingFramework
         }
 
         /// <summary>
-        /// Gets the Face for this Index
+        /// Gets the Face for this Identity
         /// </summary>
         public GambleCardFace Face
         {
@@ -105,11 +113,17 @@ namespace GamblingFramework
         /// </summary>
         /// <param name="other"></param>
         /// <returns>+ if greater, 0 if equal, - if less than.</returns>
-        public int CompareTo(IGambleCard other)
+        public int CompareTo(IStandardCard other)
         {
+            
             if (this.Face == other.Face)
             {
+                /**
+                This is a gamble card! Suit should be meaningless!
+                
                 return this.Suit.CompareTo(other.Suit);
+                */
+                return 0;
             }
             else
             {
@@ -138,9 +152,9 @@ namespace GamblingFramework
         /// <summary>
         /// Compares this to the "other" card for equality.
         /// </summary>
-        /// <param name="other"></param>
+        /// <param name="other">Other GambleCard</param>
         /// <returns>Whether other is equal to this.</returns>
-        public bool Equals(IGambleCard other)
+        public bool Equals(IStandardCard other)
         {
             return this.CompareTo(other) == 0;
         }
